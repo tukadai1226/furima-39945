61de3136b36f9c5eb4c23b95fa296d1cf26a933a
tukadai1226 <tukadai1226@icloud.com>
2023-10-19 11:23:51 +0900

    ユーザー単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :family_name, presence: true
+  validates :family_name, presence: true 
   validates :first_name, presence: true
   validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
 
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_NAME_KANA_REGEX =/\A[ァ-ヶー－]+\z/
   validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :family_name, format: { with: VALID_NAME_REGEX }
+  validates :first_name, format: { with: VALID_NAME_REGEX }
+  validates :family_name_kana, format: { with: VALID_NAME_KANA_REGEX }
+  validates :first_name_kana, format: { with: VALID_NAME_KANA_REGEX }
+
 end
