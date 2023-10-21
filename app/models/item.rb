f9f36ef9e416215989ebbbf71af975bd811a54da
tukadai1226 <tukadai1226@icloud.com>
2023-10-21 15:42:01 +0900

    商品出品機能の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,8 +10,6 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :estimated_shopping_date
 
-  NUM_REGEX = /\A[0-9]+\z/
-  validates :price, format: { with: NUM_REGEX }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
@@ -22,5 +20,5 @@ class Item < ApplicationRecord
   validates :item, presence: true, length: { maximum: 40 }
   validates :explanation, presence: true, length: { maximum: 1000 }
   validates :image, presence: true
-  validates :price, presence: true
+  validates :price, presence: true ,numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -38,32 +38,57 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Explanation is too long (maximum is 1000 characters)')
       end
       it 'カテゴリーの情報がないと出品できない' do
-        @item.category_id = ''
+        @item.category_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'カテゴリーに「---」が選択されている場合は出品できない' do
+        @item.category_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態の情報がないと出品できない' do
-        @item.condition_id = ''
+        @item.condition_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '商品の状態に「---」が選択されている場合は出品できない' do
+        @item.condition_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it '配送料の負担情報がないと出品できない' do
-        @item.burden_id = ''
+        @item.burden_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Burden can't be blank")
+      end
+      it '配送料の負担に「---」が選択されている場合は出品できない' do
+        @item.burden_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Burden can't be blank")
       end
       it '発送元の地域情報がないと出品できない' do
-        @item.prefecture_id = ''
+        @item.prefecture_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送元の地域に「---」が選択されている場合は出品できない' do
+        @item.prefecture_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it '発送までの日数情報がないと出品できない' do
-        @item.estimated_shopping_date_id = ''
+        @item.estimated_shopping_date_id = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Estimated shopping date can't be blank")
+      end
+      it '発送までの日数に「---」が選択されている場合は出品できない' do
+        @item.estimated_shopping_date_id = 1
         @item.valid?
         expect(@item.errors.full_messages).to include("Estimated shopping date can't be blank")
       end
       it '価格情報がないと出品できない' do
-        @item.price = ''
+        @item.price = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
