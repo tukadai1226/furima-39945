0805262d62486b454fd67327d3a0eba5ef7d0c01
tukadai1226 <tukadai1226@icloud.com>
2023-10-25 11:06:16 +0900

    都道府県のバリデーションの追加

--- app/models/address_history.rb
+++ app/models/address_history.rb
@@ -7,12 +7,11 @@ class AddressHistory
     validates :user_id
     validates :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :prefecture_id
     validates :city
     validates :address
     validates :telephone
   end
-
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   def save
     # 購入履歴情報を保存し、変数historyに代入する
     history = History.create(item_id: item_id, user_id: user_id)
